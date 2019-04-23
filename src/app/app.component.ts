72e559e72714dcf6c5ddb2f3712a39202966afa9
Shakiba <smorvarid@anantics.com>
2019-04-23 13:48:06 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'editable-table';
+  editField: string;
+  tableList: Array<any> = [
+    { id: 1, state: 'CA', city: 'ALBANY', county: 'ALAMEDA', taxRate: '9.75', expiration: '12/31/9999' },
+    { id: 2, state: 'CA', city: 'HAYWARD', county: 'ALAMEDA', taxRate: '9.75', expiration: '12/31/9999' },
+    { id: 3, state: 'CA', city: 'NEWARK', county: 'ALAMEDA', taxRate: '9.75', expiration: '12/31/9999' },
+    { id: 4, state: 'CA', city: 'SAN LEANARD', county: 'ALAMEDA', taxRate: '9.75', expiration: '12/31/9999' },
+    { id: 5, state: 'CA', city: 'UNION CITY', county: 'ALAMEDA', taxRate: '9.75', expiration: '12/31/9999' },
+  ];
+
+  updateList(id: number, property: string, event: any) {
+    const editField = event.target.textContent;
+    this.tableList[id][property] = editField;
+  }
+
+  changeValue(id: number, property: string, event: any) {
+    this.editField = event.target.textContent;
+  }
 }
